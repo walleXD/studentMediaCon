d479541901a2da0bd4e1986519f3999ace5fb7d9
Walee Ahmed <walee.tausif@gmail.com>
2016-04-29 18:50:00 -0400

    Add smooth scrolling in landing page and also added floating button to quickly navigate to the top

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,35 @@ $(".nav a").on("click", function(){
 // 	});
 // 	console.log('map completed!');
 // }
+
+// landing page scrool up button
+$('.goUp').hide();
+var isVisible = false;
+$(window).scroll(function(){
+     // var shouldBeVisible = true;
+     var shouldBeVisible = $(window).scrollTop()>200;
+     // console.log(shouldBeVisible);
+     if (shouldBeVisible && !isVisible) {
+          isVisible = true;
+          $('.goUp').fadeIn("slow");
+     } else if (isVisible && !shouldBeVisible) {
+          isVisible = false;
+          $('.goUp').fadeOut("slow");
+    }
+});
+
+// landingPage smooth scrool
+$(function() {
+  $('.smoothScrool a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
